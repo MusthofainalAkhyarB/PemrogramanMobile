fe172ff75b5bfd385894b9818bd88596f09b236d
amang <79791017+MusthofainalAkhyarB@users.noreply.github.com>
2023-11-11 09:01:50 +0700

    uts pemrograman mobile

---
+++ uts_pencarian_bahan_pangan/android/app/src/main/kotlin/com/example/uts_pencarian_bahan_pangan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uts_pencarian_bahan_pangan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ uts_pencarian_bahan_pangan/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UTS Pemrograman Mobile',
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UTS Pemrograman Mobile'),
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    switch (_currentIndex) {
+      case 0:
+        return const HomeScreenContent();
+      case 1:
+        return const SearchScreen();
+      default:
+        return Container(); // Handle other cases if needed
+    }
+  }
+}
+
+class HomeScreenContent extends StatelessWidget {
+  const HomeScreenContent({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+          children: [
+            Image.network(
+              'https://i2.wp.com/abouttng.com/wp-content/uploads/2018/03/Pasar-Modern.jpg?fit=1920%2C1080',
+              width: 800,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'Pasar Modern Online',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Tangerang, Indonesia',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  Text(
+                    '\nPasar Modern Online adalah platform perdagangan elektronik yang memungkinkan konsumen untuk membeli berbagai jenis bahan pangan melalui aplikasi ini. Konsep ini kami gunakan seiring meningkatnya minat konsumen dalam berbelanja bahan pangan secara online.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        // Add your existing widgets here
+      ],
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // Blank or empty container for the search screen
+  }
+}
